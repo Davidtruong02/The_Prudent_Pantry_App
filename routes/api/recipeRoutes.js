9fc371b7b22d5c161941c60edc8bd36258185dc8
dstorie80 <dstorie80@gmail.com>
2024-03-11 21:34:26 -0500

    updated the recipeRoute.js to add label, url, and ingredient list to the database.

--- routes/api/recipeRoutes.js
+++ routes/api/recipeRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const { Recipe } = require('../../models');
 
 const app_id = process.env.APP_ID;
 const app_key = process.env.APP_KEY;
+
 
 // GET /api/recipe
 router.get('/recipe', async (req, res) => {
@@ -11,7 +13,10 @@ router.get('/recipe', async (req, res) => {
 
     try {
         const response = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${q}&app_id=${app_id}&app_key=${app_key}&imageSize=REGULAR&field=label&field=image&field=url&field=ingredientLines`);
-        
+
+        console.log(response.data);
+
+
         // Extract the required fields
         const recipes = response.data.hits.map(hit => ({
             label: hit.recipe.label,
@@ -20,6 +25,15 @@ router.get('/recipe', async (req, res) => {
             ingredientLines: hit.recipe.ingredientLines
         }));
 
+        // Insert each recipe into the database
+        for (const recipe of recipes) {
+            await Recipe.create({
+                title: recipe.label,
+                url: recipe.url,
+                ingredients: JSON.stringify(recipe.ingredientLines),                
+            });
+        }
+
         // Render the template with the recipes
         res.render('recipe', { recipes });
     } catch (error) {
@@ -27,6 +41,20 @@ router.get('/recipe', async (req, res) => {
         res.status(500).send('Error fetching data from Edamam API');
     }
 });
+
+// router.post('/clearRecipes', async (req, res) => {
+//     try {
+//         await Recipe.destroy({ where: {}, truncate: false });
+//         res.json({ success: true });
+//     } catch (err) {
+//         console.error(err);
+//         res.json({ success: false });
+//     }
+// });
+
+
+
+
 
 module.exports = router;
 
