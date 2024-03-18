aea6e5f1124c1a6acf418445f4d994b92bc381ba
David Truong <Davidtruong134@gmail.com>
2024-03-17 23:26:03 -0500

    edited

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 //  Purpose: This file is used to connect to the database using Sequelize.
 require('dotenv').config();
-let sequelize;
+/* let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
@@ -15,4 +15,34 @@ if (process.env.JAWSDB_URL) {
       port: 3306,
     },
   );
-}
+} */
+
+/* let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
+} */
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  host: 'wp433upk59nnhpoh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  port: 3306,
+  database: 'uixvd8zs165ibe3x',
+  username: 't1cwfdwa5qkceyr8',
+  password: 'lz7hgpp1im6i0oxu',
+});
+
+module.exports = sequelize;
