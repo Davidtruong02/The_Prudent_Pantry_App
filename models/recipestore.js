d10e3cb576302c3eafdaca00607b87dea5452061
David Truong <Davidtruong134@gmail.com>
2024-03-17 23:03:08 -0500

    camelcased

--- models/recipestore.js
+++ models/recipestore.js
@@ -43,7 +43,7 @@ RecipeStore.init(
         timestamps: false,
         freezeTableName: true,
         underscored: true,
-        modelName: 'recipestore',
+        modelName: 'RecipeStore',
     }
 );
 
