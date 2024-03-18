9b029fc429790401d5efceb5788b494444d4b172
David Truong <Davidtruong134@gmail.com>
2024-03-17 23:16:53 -0500

    added JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,18 @@
 //  Purpose: This file is used to connect to the database using Sequelize.
 require('dotenv').config();
-const Sequelize = require('sequelize');
+let sequelize;
 
-const sequelize = new Sequelize(
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PW,
     {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306,
-        define: {
-            timestamps: false // Disable timestamps by default
-        }
-    }
-);
-
-module.exports = sequelize;
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
+}
